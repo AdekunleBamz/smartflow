4ca26e5bb9c72f04d76bcf067d9e4bd876046664
SmartFlow Developer <adekunle@smartflow.dev>
2026-01-16 18:04:50 +0100

    feat: add leaderboard components barrel export

---
+++ components/leaderboard/index.ts
@@ -0,0 +1,4 @@
+// Leaderboard components barrel export
+export { LeaderboardRow } from './LeaderboardRow';
+export { LeaderboardTable } from './LeaderboardTable';
+export { LeaderboardFiltersBar } from './LeaderboardFilters';
