a7a31a559f5053cefb7c457dc6b24b7b06f7dc95
SmartFlow Developer <adekunle@smartflow.dev>
2026-01-16 18:03:03 +0100

    feat: add dashboard components barrel export

---
+++ components/dashboard/index.ts
@@ -0,0 +1,5 @@
+// Dashboard components barrel export
+export { DashboardStat } from './DashboardStat';
+export { DashboardStatsGrid } from './DashboardStatsGrid';
+export { ActivityFeedItem } from './ActivityFeedItem';
+export { ActivityFeed } from './ActivityFeed';
