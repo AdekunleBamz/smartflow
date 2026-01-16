5663486c24001b293993db0df44d1232257d7e39
SmartFlow Developer <adekunle@smartflow.dev>
2026-01-16 18:00:37 +0100

    feat: add providers barrel export

---
+++ components/providers/index.ts
@@ -0,0 +1,4 @@
+// Provider exports
+export { QueryProvider } from './QueryProvider';
+export { ThemeProvider, useTheme } from './ThemeProvider';
+export { Web3Provider, wagmiConfig } from './Web3Provider';
