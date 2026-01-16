c35a2de8fff63289896a0086b7b861376b7e32cf
SmartFlow Developer <adekunle@smartflow.dev>
2026-01-16 18:43:12 +0100

    feat: add components barrel export

---
+++ components/index.ts
@@ -0,0 +1,29 @@
+// Components barrel export
+export * from './ui';
+
+// Dashboard components
+export { DashboardStat } from './dashboard/DashboardStat';
+export { DashboardStatsGrid } from './dashboard/DashboardStatsGrid';
+export { ActivityFeedItem } from './dashboard/ActivityFeedItem';
+export { ActivityFeed } from './dashboard/ActivityFeed';
+
+// Leaderboard components
+export { LeaderboardRow } from './leaderboard/LeaderboardRow';
+export { LeaderboardTable } from './leaderboard/LeaderboardTable';
+export { LeaderboardFilters } from './leaderboard/LeaderboardFilters';
+
+// Alert components
+export { AlertCard } from './alerts/AlertCard';
+export { CreateAlertForm } from './alerts/CreateAlertForm';
+export { AlertNotification, AlertNotificationList } from './alerts/AlertNotification';
+
+// Provider components
+export { QueryProvider } from './providers/QueryProvider';
+export { ThemeProvider, useTheme } from './providers/ThemeProvider';
+export { Web3Provider } from './providers/Web3Provider';
+
+// Core components
+export { Header } from './Header';
+export { StatCard } from './StatCard';
+export { WalletConnect } from './WalletConnect';
+export { FarcasterSDKInit } from './FarcasterSDKInit';
